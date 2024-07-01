729fab43ea10f544eb3bce79b560c60638a6f1de
Joris Nix <joris.nix@bigdata.uni-saarland.de>
2024-07-01 15:11:29 +0200

    [Benchmark] ResultDB Benchmarks.

---
+++ benchmark/result-db/algorithm/run_benchmarks.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Provide which benchmarks to execute, either 'job' or 'star'."
+    exit 1
+fi
+
+benchmark_name="$1"
+
+job_benchmarks=(
+    "q1a"
+    "q3b"
+    "q4a"
+    "q5b"
+)
+
+star_benchmarks=(
+    "star_sel_10"
+    "star_sel_20"
+    "star_sel_30"
+    "star_sel_40"
+    "star_sel_50"
+    "star_sel_60"
+    "star_sel_70"
+    "star_sel_80"
+    "star_sel_90"
+    "star_sel_100"
+)
+
+if [ ${benchmark_name} = "job" ]; then
+    for bench in "${job_benchmarks[@]}"; do
+        python3 ./benchmark/Benchmark.py ./benchmark/result-db/algorithm/job/${bench}_benchmark.yml -b build/release/ -o ./benchmark/result-db/algorithm/job/${bench}_results.csv
+    done
+elif [ ${benchmark_name} = "star" ]; then
+    for bench in "${star_benchmarks[@]}"; do
+        python3 ./benchmark/Benchmark.py ./benchmark/result-db/algorithm/star/${bench}_benchmark.yml -b ./build/release/ -o ./benchmark/result-db/algorithm/star/${bench}_results.csv
+    done
+else
+    echo "The provided benchmark must either be 'job' or 'star', ${benchmark_name} was provided."
+    exit 1
+fi
